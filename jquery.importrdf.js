eff6d9c49ce6dd846c1ecb9b7a8d221aed8abd3d
Craig Dietrich <craigdietrich@gmail.com>
2015-01-24 12:45:00 -0800

    Refactor urlform & rdfform

---
+++ jquery.importrdf.js
@@ -0,0 +1,138 @@
+/**
+ * Scalar    
+ * Copyright 2013 The Alliance for Networking Visual Culture.
+ * http://scalar.usc.edu/scalar
+ * Alliance4NVC@gmail.com
+ *
+ * Licensed under the Educational Community License, Version 2.0 
+ * (the "License"); you may not use this file except in compliance 
+ * with the License. You may obtain a copy of the License at
+ * 
+ * http://www.osedu.org/licenses/ECL-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.       
+ */  
+
+/**
+ * @projectDescription  Import RDF-JSON to a Scalar book
+ * @author              Michael Lynch
+ * @author				Craig Dietrich
+ * @version             1.0
+ */
+
+(function($) {
+
+	var defaults = {
+			dest_url: '',
+			relations: {
+			  'tag':{},
+			  'path':{},
+			  'reply':{},
+			  'anno':{}
+			}		
+	};
+	var opts = {};
+	
+	var rdfimporter_methods = {
+			
+		init : function(options) {
+			opts = $.extend( {}, defaults, options );
+			return this.each(function() {
+				var $this = $(this);
+				opts['$this'] = $this;
+			});	 
+		},
+		book_rdf : function(options, callback) {
+			var url = options.url.replace(/\/$/, "")+'/rdf?format=json&callback=?';
+			$.getJSON(url, function(rdf) {
+				for (var uri in rdf) break;  // First node
+				if ('http://scalar.usc.edu/2012/01/scalar-ns#Book' != rdf[uri]['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'][0].value) {
+					callback();
+					return;
+				}
+				callback(rdf[uri]);
+			});
+		},		
+		rdf : function(options, callback) {
+			// RDF-JSON string
+			if ('undefined'!=typeof(options.rdf)) {
+				var rdf = options.rdf;
+				try {
+					rdf = JSON.parse(rdf);
+				} catch (e) {
+					callback();
+					return;
+				}
+				callback(rdf);
+			// URL to RDF-JSON
+			} else if ('undefined'!=typeof(options.url)) {
+				var url = options.url+'/rdf/instancesof/content?rec=1&ref=1&format=json&callback=?';
+			    $.getJSON( url, function(rdf) {
+			    	callback(rdf);
+			    }).fail(function() {
+			        callback();
+			    });			
+			} else {
+				callback();
+			}
+		},	
+		rdf_value : function(options) {
+			var rdf = options.rdf;
+			var p = options.p;
+			var value = rdf[p][0].value;
+			return value;
+		},		
+		import : function(options, callback) {
+			switch(options.form) {
+				case 'urlform':
+					$.fn.rdfimporter('import_by_url', options, function(rdf) {
+						opts.source_rdf = rdf;
+						$.fn.rdfimporter('source', {source:JSON.stringify(rdf, null, 1)});
+						callback();
+					});
+					break;
+				case 'rdfform':
+					$.fn.rdfimporter('import_by_rdf', options);
+					break;
+				default:
+					alert('Could not determine the type of form');
+			}
+		},
+		import_by_url : function(options, callback) {
+			var source_url = opts.source_url.replace(/\/$/, "");
+			$.fn.rdfimporter('rdf', {source_url:source_url}, function(rdf) {
+				callback(rdf);
+			});
+		},
+		import_by_rdf : function(options) {
+			var rdf = opts.fields.rdfForm.rdf.val();
+			options = {dest_id:opts.fields.rdfForm.dest_id.val(),dest_url:opts.fields.rdfForm.dest_url.val()};
+			$.fn.rdfimporter('confirm_book', options, function(dest_urn) {
+				options.dest_urn = dest_urn;
+				$.fn.rdfimporter('browse', options, rdf);
+			});
+		},
+		save : function(options, callback) {
+			$.fn.rdfimporter('action', {action:'Results of each ajax action here ...'});
+			callback();
+		}
+	};
+
+	$.fn.rdfimporter = function(methodOrOptions) {		
+
+		if ( rdfimporter_methods[methodOrOptions] ) {
+			return rdfimporter_methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
+			// Default to "init"
+			return rdfimporter_methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.rdfimporter' );
+		}    
+		
+	};
+	
+})(jQuery);
---
+++ main.js
@@ -0,0 +1,128 @@
+$(document).ready(function() {
+
+	var $wrapper = $('#wrapper');
+	
+	// Commit user input
+	var commit = function() {
+		if ('undefined'==typeof(commit.active)) commit.active = 0;
+		commit.active++;
+		if (commit.active<2) return;
+		console.log('commit.active: '+commit.active);
+		$commitform = $('#commitform');
+		$source_rdf = $commitform.find('#source_rdf');
+		$source_rdf.val('Loading ...');
+		$modal = $commitform.closest('.modal');
+		$modal.find('.book_title').html($commitform.find('#dest_title').val());
+		$modal.modal('show');
+		$modal.find('button').attr('disabled', 'disabled');
+		$modal.find('button.cancel').click(function() {
+			$modal.modal('hide');
+			commit.active = 0;
+		});
+		$modal.find('button[type="submit"]').click(function() {
+			alert('Nothing yet ...');
+		});
+		if ($commitform.data('rdf')) {
+			$source_rdf.val(JSON.stringify($commitform.data('rdf'), null, 1));
+			commit.active = 0;
+			$modal.find('button').removeAttr('disabled');
+		} else {
+			$.fn.rdfimporter('rdf', {url:$commitform.find('#source_url').val()}, function(rdf) {
+				if ('undefined'==typeof(rdf) || !rdf) {
+					$source_rdf.val('Could not retrieve source RDF-JSON.  Please try again.');
+					return;
+				}
+				$source_rdf.val(JSON.stringify(rdf, null, 1));
+				$commitform.data('rdf', rdf);
+				commit.active = 0;
+				$modal.find('button').removeAttr('disabled');
+			});
+		}
+	}
+
+	// Validate user input from url form
+	$('#urlform').submit(function() {
+		commit.active = 0;
+		var $form = $(this);
+		var $commitform = $('#commitform');
+		$commitform.removeData('rdf');
+		var $source_msg = $form.find('.source_msg');
+		var $dest_msg = $form.find('.dest_msg');
+		var source_url = $form.find('.source_url').val();
+		var dest_url = getURLParameter('dest_url');
+		$source_msg.html('Loading source book ...').parent().removeClass('alert-danger').addClass('alert-success').fadeIn();
+		$dest_msg.html('Loading destination book ...').parent().removeClass('alert-danger').addClass('alert-success').fadeIn();
+		$.fn.rdfimporter('book_rdf', {url:source_url}, function(rdf) {
+			if ('undefined'==typeof(rdf) || !rdf) {
+				$source_msg.html('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Could not find source book!').parent().removeClass('alert-success').addClass('alert-danger');
+				return;
+			}
+			var title = $.fn.rdfimporter('rdf_value',{rdf:rdf,p:'http://purl.org/dc/terms/title'});
+			var urn = $.fn.rdfimporter('rdf_value',{rdf:rdf,p:'http://scalar.usc.edu/2012/01/scalar-ns#urn'});			
+			$source_msg.html('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Source book <b title="'+urn+'">'+title+'</b> found!');
+			$commitform.find('#source_url').val(source_url);
+			commit();
+		});
+		$.fn.rdfimporter('book_rdf', {url:dest_url}, function(rdf) {
+			if ('undefined'==typeof(rdf) || !rdf) {
+				$dest_msg.html('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Could not find destination book!').parent().removeClass('alert-success').addClass('alert-danger');;
+				return;
+			}
+			var title = $.fn.rdfimporter('rdf_value',{rdf:rdf,p:'http://purl.org/dc/terms/title'});
+			var urn = $.fn.rdfimporter('rdf_value',{rdf:rdf,p:'http://scalar.usc.edu/2012/01/scalar-ns#urn'});
+			$dest_msg.html('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Destination book <b title="'+urn+'">'+title+'</b> found!');
+			$commitform.find('#dest_url').val(dest_url);
+			$commitform.find('#dest_urn').val(urn);
+			$commitform.find('#dest_id').val($form.find('.dest_id').val());
+			$commitform.find('#dest_title').val(title);
+			commit();
+		});		
+		return false;
+	});
+
+	// Validate user input from rdf form
+	$('#rdfform').submit(function() {
+		commit.active = 0;
+		var $form = $(this);
+		var $commitform = $('#commitform');
+		$commitform.removeData('rdf');
+		var $source_msg = $form.find('.source_msg');
+		var $dest_msg = $form.find('.dest_msg');
+		var source_rdf = $form.find('.source_rdf').val();
+		var dest_url = getURLParameter('dest_url');
+		$source_msg.html('Loading source RDF ...').parent().removeClass('alert-danger').addClass('alert-success').fadeIn();
+		$dest_msg.html('Loading destination book ...').parent().removeClass('alert-danger').addClass('alert-success').fadeIn();
+		$.fn.rdfimporter('rdf', {rdf:source_rdf}, function(rdf) {
+			if ('undefined'==typeof(rdf) || !rdf) {
+				$source_msg.html('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Invalid RDF-JSON!').parent().removeClass('alert-success').addClass('alert-danger');;
+				return;
+			}			
+			$source_msg.html('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Valid RDF-JSON!');
+			$commitform.find('#source_rdf').val(JSON.stringify(rdf, null, 1));
+			$commitform.data('rdf', rdf);
+			commit();
+		});
+		$.fn.rdfimporter('book_rdf', {url:dest_url}, function(rdf) {
+			if ('undefined'==typeof(rdf) || !rdf) {
+				$dest_msg.html('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Could not find destination book!').parent().removeClass('alert-success').addClass('alert-danger');;
+				return;
+			}
+			var title = $.fn.rdfimporter('rdf_value',{rdf:rdf,p:'http://purl.org/dc/terms/title'});
+			var urn = $.fn.rdfimporter('rdf_value',{rdf:rdf,p:'http://scalar.usc.edu/2012/01/scalar-ns#urn'});
+			$dest_msg.html('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Destination book <b title="'+urn+'">'+title+'</b> found!');
+			$commitform.find('#dest_url').val(dest_url);
+			$commitform.find('#dest_urn').val(urn);
+			$commitform.find('#dest_id').val($form.find('.dest_id').val());
+			$commitform.find('#dest_title').val(title);
+			commit();
+		});				
+		return false;
+	});	
+	
+});
+
+function getURLParameter(name) {
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+}
